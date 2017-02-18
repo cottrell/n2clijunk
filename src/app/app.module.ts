0a018ce7f00cc5c6b173203b4ac9d5e20ceb6fd4
David Cottrell <cottrell@users.noreply.github.com>
2017-02-18 11:42:15 +0000

    tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TabComponent } from './tab/tab.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
